80767b8289850e8d46ba99bd48c8d820a6f93e49
kuszot <kuszot123@gmail.com>
2025-04-28 22:10:24 +0200

    create Leave Requests page component

---
+++ app/manager/leave-requests/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LeaveRequestsPageContent from './LeaveRequestsPageContent'
+
+const page = () => {
+  return (
+    <LeaveRequestsPageContent/>
+  )
+}
+
+export default page
