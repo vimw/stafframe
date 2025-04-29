da8093fd76bae7a58cd031d0d32da2ff1c49bf95
kuszot <kuszot123@gmail.com>
2025-04-29 15:01:36 +0200

    Add Chart component for leave requests visualization

--- app/manager/leave-requests/LeaveRequestsPageContent.tsx
+++ app/manager/leave-requests/LeaveRequestsPageContent.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import styles from './LeaveRequestsPageContent.module.css'
 import RequestsStatusCard from './components/RequestsStatusCard';
+import LeaveRequestsChart from './components/LeaveRequestsChart';
 
 const LeaveRequestsPageContent = () => {
     const [currentTab,setCurrentTab] = useState<'Requests' | 'History'>('Requests');
@@ -28,13 +29,15 @@ const LeaveRequestsPageContent = () => {
                     </h2>
                 </div>
                 <div className={styles.stats}>
-                    <div className={styles.charts}>f</div>
+                    <div className={styles.charts}>
+                        <LeaveRequestsChart title="Weekly Leave Requests Overview" dataValues={[1,3,6,8,3,1,7]} labels={['Mon','Tue','Wed','Thu','Fri','Sat','Sun']}/>
+                        <LeaveRequestsChart title="Monthly Leave Requests Overview" dataValues={[1,3,6,8,3,1,7,9,3,6,2,32]} labels={['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dec']}/>
+                    </div>
                     <div className={styles.requestsStatusCards}>
                         <RequestsStatusCard status="Approved" count={3} icon='/approve_circle.svg'/>
                         <RequestsStatusCard status="Declined" count={2} icon='/decline_circle.svg'/>
                         <RequestsStatusCard status="Pending" count={7} icon='/pending_circle.svg'/>
                         <RequestsStatusCard status="Requests" count={12} icon='/requests_circle.svg'/>
-
                     </div>
                 </div>
             </div>
