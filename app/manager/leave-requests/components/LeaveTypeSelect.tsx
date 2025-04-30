b8c4935874124a41f5af1a88c63885143d34d96c
kuszot <kuszot123@gmail.com>
2025-04-30 09:59:39 +0200

    Add Leave Type Select component

---
+++ app/manager/leave-requests/components/LeaveTypeSelect.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Select } from 'antd';
+import type { SelectProps } from 'antd';
+
+
+interface LeaveTypeSelectProps{
+    leaveTypes: { label: string; value: string }[];
+}
+
+const options: SelectProps['options'] = [];
+
+
+const LeaveTypeSelect = ({leaveTypes}: LeaveTypeSelectProps) => {
+  return (
+    <Select
+        mode="multiple"
+        allowClear
+        style={{ width: '100%' }}
+        placeholder="Leave Type"
+        options={leaveTypes}
+    />
+  )
+}
+
+export default LeaveTypeSelect
