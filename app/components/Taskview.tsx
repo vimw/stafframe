37ac5775021242820e6961679a73ba6799f6d195
vimw <xavobudzio@gmail.com>
2025-05-04 19:58:24 +0200

    moved css variables to js for clarity

--- app/components/Taskview.tsx
+++ app/components/Taskview.tsx
@@ -7,23 +7,33 @@ const hourRange = {
     endAt: 23,
     steps: 2
 };
-const hourBoxSize = 120      // px
-const hourBoxBorder = 1     // px
+const hourBoxSize = 120                                                                     // px
+const hourBoxBorder = 1                                                                     // px
 const boxSize = hourBoxSize / hourRange.steps - hourBoxBorder;
 const boxNumber = (hourRange.endAt - hourRange.startAt + 1) * hourRange.steps;
-const tasktabwidth = 200;
+const taskTabWidth = 200;                                                                   // px
+const taskViewPadding = 4;                                                                  // px
+const headerSize = 64;                                                                      // px
 
 function Taskview({ children }: { children?: React.ReactNode }){
     return (
-        <div className={styles.taskview}>
-            <div className={styles.bg}>
+        <div className={styles.taskview} style={{
+            padding: taskViewPadding
+        }}>
+            <div className={styles.bg} style={{
+                top: taskViewPadding + headerSize,
+                left: taskViewPadding
+            }}>
                 {Array.from({ length: boxNumber }).map((_, i) => (
-                    <div key={i} style={{height: boxSize, borderTop: `${hourBoxBorder}px solid #AFAFAF`}}></div>
+                    <div key={i} style={{
+                        height: boxSize,
+                        borderTop: `${hourBoxBorder}px solid #AFAFAF`
+                    }}></div>
                 ))}
             </div>
             <div className={styles.content}>
                 <Taskviewtimedisplay />
-                <Splitter>
+                <Splitter style={{flexGrow: 1, width: "0", height: "100%"}}>
                     {children}
                 </Splitter>
             </div>
@@ -33,11 +43,15 @@ function Taskview({ children }: { children?: React.ReactNode }){
 
 function Taskviewtimedisplay(){
     return (
-        <div className={styles.hourdisplay}>
+        <div className={styles.hourdisplay} style={{
+            marginTop: headerSize
+        }}>
             {Array.from({ length: boxNumber}).map((_, i) => {
                 let min = (i + hourRange.startAt) * (60 / hourRange.steps);
                 return (
-                    <div key={i} style={{height: boxSize}}>{Math.floor(min/60)}:{min%60 < 10 ? `0${min%60}` : min%60}</div>
+                    <div key={i} style={{
+                        height: boxSize
+                    }}>{Math.floor(min/60)}:{min%60 < 10 ? `0${min%60}` : min%60}</div>
                 );
             })}
         </div>
@@ -46,8 +60,11 @@ function Taskviewtimedisplay(){
 
 function TaskTab({ title="New Tab", children }: { title?: string, children?: React.ReactNode }){
     return (
-        <Splitter.Panel className={styles.tab} defaultSize={tasktabwidth}>
-            <div className={styles.tabtop}>
+        <Splitter.Panel className={styles.tab} defaultSize={taskTabWidth}>
+            <div className={styles.tabtop} style={{
+                top: taskViewPadding,
+                height: headerSize
+            }}>
                 <h2>{title}</h2>
             </div>
             <div className={styles.tabtasks}>
@@ -57,7 +74,7 @@ function TaskTab({ title="New Tab", children }: { title?: string, children?: Rea
     );
 }
 
-function Task({ title="New Task", children }: { title?:string, children?: React.ReactNode }){
+function Task({ title="New Task", start="8:00", length=60, children }: { title?:string, start?:string, length?: number, children?: React.ReactNode }){
     return (
         <div className={styles.task}>
             <h3>{ title }</h3>
