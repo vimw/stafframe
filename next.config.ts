8a2e5ee2f3cafb856a40d784c203198934135099
vimw <61950682+vimw@users.noreply.github.com>
2025-05-05 10:15:25 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  // Optional: Add this if you use next/image
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
