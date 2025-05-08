5de2abef0a79157e24074e3c8c5f171648863220
vimw <xavobudzio@gmail.com>
2025-05-09 01:34:40 +0200

    File cleanup

--- app/components/Taskview.tsx
+++ app/components/Taskview.tsx
@@ -1,50 +1,13 @@
 import React from "react";
 import { Splitter } from "antd";
 import styles from "./Taskview.module.css";
-
-interface taskTabI {
-    title?: string,
-    tasks?: taskI[]
-}
-
-interface taskI {
-    title?: string,
-    desc?: string,
-    bgcolor?: string,
-    taskTime?: taskTimeI
-}
-
-interface taskTimeI {
-    hour: number,
-    minute: number,
-    length: number
-}
+import { taskTabI, taskI, taskTimeI } from "@/lib/tasks/TaskDefinitions";
 
 const hourRange = {
     startAt: 0,
     endAt: 23,
     steps: 2
 };
-
-const defaultTaskTime: taskTimeI = {
-    hour: hourRange.startAt,
-    minute: 0,
-    length: 60
-}
-
-function verifyTaskTime(taskTime: taskTimeI){
-    // verify if its in hourRange
-    if(taskTime.hour < hourRange.startAt || taskTime.hour > hourRange.endAt){
-        return false;
-    }
-
-    // verify if minutes are ok
-    if(taskTime.minute < 0 || taskTime.minute > 59){
-        return false;
-    }
-
-    return true;
-}
 
 const hourBoxSize = 120;                                                                    // px
 const boxBorder = 1;                                                                        // px
@@ -115,56 +78,6 @@ function TaskTab({ title="New Tab", children }: { title?: string, children?: Rea
     );
 }
 
-function Task({ title="New Task", bgcolor="red", taskTime=defaultTaskTime, children }: { title?: string, bgcolor?: string, taskTime?: taskTimeI, children?: React.ReactNode }){
-    // calculate if the provided taskTime is correct,
-    // otherwise fallback to default
-    if(!verifyTaskTime(taskTime)){
-        taskTime = defaultTaskTime;
-    }
-
-    // calculate the top
-    let top: number = 0;
-    top += taskTime.hour * hourBoxSize;
-    top += taskTime.minute * (hourBoxSize / 60);
-
-    // calculate the size
-    let size: number = taskTime.length * (hourBoxSize / 60);
-
-    return (
-        <div className={styles.task} style={{
-            top: top,
-            height: size,
-            backgroundColor: bgcolor
-        }}>
-            <h3>{ title }</h3>
-            <p>{ children }</p>
-        </div>
-    );
-}
-
-
-// extras
-function createTabsFromObject(tabs: taskTabI[]){
-    return (
-        <>
-        {tabs.map((tab, tabIndex) => (
-            <TaskTab key={tabIndex} title={tab.title ?? undefined}>
-                { tab.tasks?.map((task, taskIndex) => (
-                    <Task key={taskIndex} title={task.title ?? undefined} bgcolor={task.bgcolor ?? undefined} taskTime={task.taskTime ?? undefined}>
-                        { task.desc }
-                    </Task>
-                )) }
-            </TaskTab>
-        ))}
-        </>
-    );
-}
 
 // components
-export { Taskview, TaskTab, Task };
-
-// functions
-export { createTabsFromObject };
-
-// interfaces
-export type { taskTabI, taskI, taskTimeI };
+export { Taskview, TaskTab };
