b8b6f7fa17de1e249f3d400fb62d29036efefd67
Jakub Szot <88439338+kuszot@users.noreply.github.com>
2025-05-09 13:04:54 +0200

    Update page.tsx

--- app/manager/leave-requests/[id]/page.tsx
+++ app/manager/leave-requests/[id]/page.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
+import Header from './components/Header/Header'
+import styles from './page.module.css'
+import Details from './components/details/Details'
+import ManagerDecision from './components/ManagerDecision/ManagerDecision'
+import EmployeeInfo from './components/EmployeeInfo/EmployeeInfo'
 
 interface Props {
   params: Promise<{id: number}>
 }
 
-
 const page = async ({params}: Props) => {
   const {id} = await params
-
   return (
-    <div>{id}</div>
+    <section className={styles.detailsContainer}>
+      <div className={styles.detailsContent}>
+        <Header/>
+        <div className={styles.flexWrapper}>
+          <div className={styles.detailsAndDecisionWrapper}>
+            <Details/>
+            <ManagerDecision/>
+          </div>
+          <div className={styles.employeeInfo}>
+            <EmployeeInfo/>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
