165b9b9eab782048cf44f9956627d04973ce6ce8
kuszot <kuszot123@gmail.com>
2025-05-14 22:28:33 +0200

    Handle login and errors

--- app/(auth)/login/LoginForm.tsx
+++ app/(auth)/login/LoginForm.tsx
@@ -1,30 +1,15 @@
 "use client"
 import type React from "react"
 
-import { useState } from "react"
+import { useActionState } from "react"
 import styles from "./login.module.css"
 import Link from "next/link"
+import { login } from "./actions"
+import { useFormStatus } from "react-dom"
 
 const LoginForm = () => {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-    rememberMe: false,
-  })
-  const [error,setError] = useState<string>('')
-  
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value, type, checked } = e.target
-    setFormData((prev) => ({
-      ...prev,
-      [name]: type === "checkbox" ? checked : value,
-    }))
-  }
+  const [state, loginAction] = useActionState(login, undefined);
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    
-  }
 
   return (
     <div className={styles.loginContainer}>
@@ -38,7 +23,7 @@ const LoginForm = () => {
         <h1 className={styles.title}>Sign in to your account</h1>
         <p className={styles.subtitle}>Welcome back! Please enter your details.</p>
 
-        <form className={styles.form} onSubmit={handleSubmit}>
+        <form action={loginAction} className={styles.form}>
           <div className={styles.formGroup}>
             <label htmlFor="email" className={styles.label}>
               Email
@@ -49,11 +34,12 @@ const LoginForm = () => {
               name="email"
               className={styles.input}
               placeholder="Enter your email"
-              value={formData.email}
-              onChange={handleChange}
               required
             />
           </div>
+          {state?.errors?.email && (
+            <p className={styles.error}>{state.errors.email}</p>
+          )}
 
           <div className={styles.formGroup}>
             <div className={styles.passwordHeader}>
@@ -70,32 +56,24 @@ const LoginForm = () => {
               name="password"
               className={styles.input}
               placeholder="Enter your password"
-              value={formData.password}
-              onChange={handleChange}
               required
             />
           </div>
 
-          <div className={styles.checkboxContainer}>
-            <label className={styles.checkboxLabel}>
-              <input
-                type="checkbox"
-                name="rememberMe"
-                checked={formData.rememberMe}
-                onChange={handleChange}
-                className={styles.checkbox}
-              />
-              <span className={styles.checkboxText}>Remember me</span>
-            </label>
-          </div>
-
-          <button type="submit" className={styles.loginButton}>
-            Sign in
-          </button>
+          <SubmitButton />
         </form>
       </div>
     </div>
   )
+}
+
+function SubmitButton() {
+    const {pending} = useFormStatus();
+    return (
+        <button disabled={pending} type="submit" className={styles.loginButton}>
+            Login
+        </button>
+    )
 }
 
 export default LoginForm
