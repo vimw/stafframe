4e8cc4b975035c297043a7a737121e6f392ca0dc
unknown <kuszot123@gmail.com>
2025-05-15 21:55:46 +0200

    Add manage users table component and styling

---
+++ app/manager/manage-users/components/ManageUsersTable/ManageUsersTable.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import styles from './ManageUsersTable.module.css'
+import ManageUsersRow from '../ManageUsersRow/ManageUsersRow'
+
+
+interface Users{
+    id:string,
+    name: string
+    email: string,
+    department:string,
+    position: string,
+}
+
+interface ManageUsersTableProps{
+    header: string[]
+    data: Users[]
+}
+
+const ManageUsersTable = ({header,data}: ManageUsersTableProps) => {
+  return (
+    <div className={styles.wrapper}>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            {header.map((element, index) => (
+              <th key={index} className={styles.th} style={{width: 100/header.length}}>
+                {element}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((user) => (
+            <ManageUsersRow key={user.id} user={user} columnCount={data.length} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default ManageUsersTable
