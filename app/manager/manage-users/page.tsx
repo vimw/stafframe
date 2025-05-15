e93fa3cc5775e29e905fb99057018c4428eda3ca
unknown <kuszot123@gmail.com>
2025-05-15 14:22:59 +0200

    Add manage users page file

---
+++ app/manager/manage-users/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ManageUsersPageContent from './ManageUsersPageContent'
+
+const page = () => {
+  return (
+    <ManageUsersPageContent />
+  )
+}
+
+export default page
