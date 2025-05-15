f18ba0b18854bccc0b48d7b0fead081938bcec3a
unknown <kuszot123@gmail.com>
2025-05-15 14:21:48 +0200

    Add link to manage users directory

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
                   <Link href="/">Home</Link>
                 </li>
                 <li>
-                  <Link href="/">Manage Users</Link>
+                  <Link href="/manager/manage-users">Manage Users</Link>
                 </li>
                 <li>
                   <Link href="/manager/leave-requests">Leave Requests</Link>
