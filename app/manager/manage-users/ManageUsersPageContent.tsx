eb5d6bcb2b55145ff285bc6ce63c858768f2b81c
kuszot <kuszot123@gmail.com>
2025-05-17 19:25:16 +0200

    Ad fetching Users and handle loading state

--- app/manager/manage-users/ManageUsersPageContent.tsx
+++ app/manager/manage-users/ManageUsersPageContent.tsx
@@ -1,28 +1,45 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './ManageUsersPageContent.module.css'
 import EmployeeSearchInput from '../leave-requests/components/EmployeeSearchInput/EmployeeSearchInput'
 import ManageUsersTable from './components/ManageUsersTable/ManageUsersTable'
+import { fetchUsers } from './lib/users'
 
+
+interface User{
+  id:string,
+  name:string,
+  email:string,
+  department:string,
+  position:string,
+  joinDate:string
+}
 
 const ManageUsersPageContent = () => {
   
     const [filteredEmployees,setFilteredEmployees] = useState<string[]>([])
+    const [users,setUsers] = useState<User[][]>([])
+    const [usersCount,setUsersCount] = useState<number>(0)
+    const [currentPage,setCurrentPage] = useState<number>(1)
+    const [loading,setLoading] = useState<boolean>(true)
+
+    const pageSize = 6
+
+    useEffect(() => {
+      setLoading(true)
+      const fetchData = async () => {
+        const {paginatedUsers,totalCount} = await fetchUsers(filteredEmployees,currentPage,pageSize)
+        setUsers((prev) => [...prev,paginatedUsers])
+        setUsersCount(totalCount)
+        setLoading(false)
+      }
+      fetchData();
+    },[currentPage,filteredEmployees])
     
 
     function handleFilterEmployees(users: string[]){
         setFilteredEmployees(users)
     }
-
-    const users = [
-        {
-            id: '1',
-            name: 'Jan Kowalski',
-            email: 'jan.kowalski@stafframe.com',
-            department: 'Finance',
-            position: 'Financial Analyst',
-        }
-    ]
   
   return (
     <section className={styles.container}>
@@ -35,7 +52,9 @@ const ManageUsersPageContent = () => {
               <EmployeeSearchInput handleFilterEmployees={handleFilterEmployees}/>
             </div>
             <div className={styles.manageUsersTable}>
-              <ManageUsersTable header={["Name","Email","Department","Position","Actions"]} data={users}/>
+              {loading || !users[currentPage-1] ? null: (
+                  <ManageUsersTable header={["Name","Email","Department","Position","Join Date","Actions"]} data={users[currentPage-1]}/>
+              )}
             </div>
 
         </div>
