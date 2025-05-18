ef57a711628361d593980a03bd16433c9aaffa95
kuszot <kuszot123@gmail.com>
2025-05-18 14:42:11 +0200

    Handle click

--- app/manager/manage-users/components/ActionButton/ActionButton.tsx
+++ app/manager/manage-users/components/ActionButton/ActionButton.tsx
@@ -1,13 +1,39 @@
 import React from 'react'
 import styles from './ActionButton.module.css'
 
-interface ActionButtonProps{
-    action: "Edit" | "Delete"
+interface User{
+    id:string,
+    name: string
+    email: string,
+    department:string,
+    position: string,
+    joinDate: string,
+    leaveBalance: {
+      annual: number
+      sick: number
+      personal: number
+    }
 }
 
-const ActionButton = ({action}: ActionButtonProps) => {
+interface ActionButtonProps{
+    action: "Edit" | "Delete",
+    user: User,
+    onEdit?: (user:User) => void
+    onDelete?: (user:User) => void
+}
+
+const ActionButton = ({action,user,onEdit,onDelete}: ActionButtonProps) => {
+
+  function handleClick(){
+    if(action === 'Edit' && onEdit){
+      onEdit(user)
+    } else if (action === 'Delete' && onDelete){
+      onDelete(user)
+    }
+  }
+
   return (
-    <button className={`${styles.actionButton} ${action === 'Delete' ? styles.deleteButton: ''}`}>
+    <button className={`${styles.actionButton} ${action === 'Delete' ? styles.deleteButton: ''}`} onClick={handleClick}>
         {action}
     </button>
   )
