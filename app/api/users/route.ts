e8b7bc0b53379db5582978781dd2b15b3262c021
kuszot <kuszot123@gmail.com>
2025-05-18 16:26:34 +0200

    Replace mock users with db users

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,58 +1,40 @@
+import { connectDB } from '@/lib/db/db';
+import User from '@/models/User';
 import { NextResponse } from 'next/server';
 
-const mockUsers = [
-  { id: '1', name: 'Anna Kowalska', avatar: '/profile_picture.jpg',email: 'anna.kowalska@stafframe.com',department: 'Engineering',position: 'Software Developer', joinDate: '2020-01-23',leaveBalance: {annual:15,personal:15,sick:10} },
-  { id: '2', name: 'Jan Nowak', avatar: '/profile_picture.jpg',email: 'jan.nowak@stafframe.com',department: 'Engineering',position: 'Software Developer', joinDate: '2020-01-23',leaveBalance: {annual:15,personal:15,sick:10} },
-  { id: '3', name: 'Piotr Zieliński', avatar: '/profile_picture.jpg',email: 'piotr.zielinski@stafframe.com',department: 'Engineering',position: 'Software Developer', joinDate: '2020-01-23',leaveBalance: {annual:15,personal:15,sick:10} },
-  { id: '4', name: 'Maria Wiśniewska', avatar: '/profile_picture.jpg',email: 'maria.wiesniewska@stafframe.com',department: 'Engineering',position: 'Software Developer', joinDate: '2020-01-23',leaveBalance: {annual:15,personal:15,sick:10} },
-  { id: '5', name: 'Tomasz Kaczmarek', avatar: '/profile_picture.jpg',email: 'tomasz.kaczmarek@stafframe.com',department: 'Engineering',position: 'Software Developer', joinDate: '2020-01-23',leaveBalance: {annual:15,personal:15,sick:10} },
-  { id: '6', name: 'Katarzyna Wójcik', avatar: '/profile_picture.jpg',email: 'katarzyna.wojcik@stafframe.com',department: 'Engineering',position: 'Software Developer', joinDate: '2020-01-23',leaveBalance: {annual:15,personal:15,sick:10} },
-  { id: '7', name: 'Michał Lewandowski', avatar: '/profile_picture.jpg',email: 'michal.lewandowski@stafframe.com',department: 'Engineering',position: 'Software Developer', joinDate: '2020-01-23',leaveBalance: {annual:15,personal:15,sick:10} },
-  { id: '8', name: 'Natalia Kamińska', avatar: '/profile_picture.jpg',email: 'natalia.kaminska@stafframe.com',department: 'Engineering',position: 'Software Developer', joinDate: '2020-01-23',leaveBalance: {annual:15,personal:15,sick:10} },
-  { id: '9', name: 'Adam Nowicki', avatar: '/profile_picture.jpg',email: 'adam.nowicki@stafframe.com',department: 'Engineering',position: 'Software Developer', joinDate: '2020-01-23',leaveBalance: {annual:15,personal:15,sick:10} },
-  { id: '10', name: 'Ewa Mazur', avatar: '/profile_picture.jpg',email: 'ewa.mazur@stafframe.com',department: 'Engineering',position: 'Software Developer', joinDate: '2020-01-23',leaveBalance: {annual:15,personal:15,sick:10} },
-  { id: '11', name: 'Kamil Dąbrowski', avatar: '/profile_picture.jpg',email: 'kamil.dabrowski@stafframe.com',department: 'Engineering',position: 'Software Developer', joinDate: '2020-01-23',leaveBalance: {annual:15,personal:15,sick:10} },
-  { id: '12', name: 'Agnieszka Piotrowska', avatar: '/profile_picture.jpg',email: 'agnieszka.piotrowska@stafframe.com',department: 'Engineering',position: 'Software Developer', joinDate: '2020-01-23',leaveBalance: {annual:15,personal:15,sick:10} },
-
-];
-
-interface data{
-    id: string,
-    name:string,
-    email:string,
-    department:string,
-    position:string,
-    joinDate:string
-    leaveBalance: {
-        annual: number
-        sick: number
-        personal: number
-    }
-}
-
-const paginateData = (data: data[], page: number, pageSize: number) => {
-    const start = (page - 1) * pageSize;
-    const end = page * pageSize;
-    return data.slice(start, end);
-};
 
 export async function GET(request: Request){
-    const { searchParams } = new URL(request.url)
-    const search = searchParams.getAll('search') || '';
-    const page = parseInt(searchParams.get('page') || '1',10)
-    const pageSize = parseInt(searchParams.get('pageSize') || '1',10)
+    try {
+        await connectDB()
+        const { searchParams } = new URL(request.url)
+        const search = searchParams.getAll('search') || '';
+        const page = parseInt(searchParams.get('page') || '1',10)
+        const pageSize = parseInt(searchParams.get('pageSize') || '1',10)
+        console.log(search)
     
-    let filtered = mockUsers
+        const query = search.length > 0 ? {
+            _id: {$in: search}
+        } : {}
 
-    if(search.length > 0){
-        filtered = filtered.filter((user) => 
-            search.includes(user.id)
-        )
+        console.log(query)
+    
+        const totalCount = await User.countDocuments(query)
+        const users = await User.find(query)
+            .skip((page -1) * pageSize)
+            .limit(pageSize)
+            .lean()
+            .select('-password')
+
+        const usersWithId = users.map((user) => ({
+            ...user,
+            id: user._id,
+            _id: undefined
+        }))
+
+        return NextResponse.json({paginatedUsers:usersWithId,totalCount})
+
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        return NextResponse.json({ error: 'Server error' }, { status: 500 });
     }
-
-    const paginatedUsers = paginateData(filtered,page,pageSize)
-    return NextResponse.json({
-            paginatedUsers,
-            totalCount: filtered.length
-    })
 }
