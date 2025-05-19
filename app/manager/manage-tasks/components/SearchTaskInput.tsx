9feed42bce4c622b6d89f5b15b60ac87e2a01455
kuszot <kuszot123@gmail.com>
2025-05-19 16:13:01 +0200

    Add SearchTaskInput component

---
+++ app/manager/manage-tasks/components/SearchTaskInput.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Input } from 'antd';
+
+const SearchTaskInput = () => {
+  return (
+    <Input placeholder='Search tasks...' />
+  )
+}
+
+export default SearchTaskInput
