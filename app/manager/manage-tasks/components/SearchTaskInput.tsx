f7b659d02dc6a5968a86ccf7bb6a669208b86d6e
kuszot <kuszot123@gmail.com>
2025-05-19 21:16:15 +0200

    Add logic to search task input compoennt

--- app/manager/manage-tasks/components/SearchTaskInput.tsx
+++ app/manager/manage-tasks/components/SearchTaskInput.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Input } from 'antd';
 
-const SearchTaskInput = () => {
+interface Props {
+  onSearch: (searchTerm: string) => void;
+}
+
+const SearchTaskInput = ({onSearch} : Props) => {
+  const [searchValue, setSearchValue] = useState<string>('');
+  const [debouncedValue, setDebouncedValue] = useState<string>(searchValue);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(searchValue);
+    }, 300);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [searchValue]);
+
+  useEffect(() => {
+    onSearch(debouncedValue);
+  }, [debouncedValue, onSearch]);
+  
   return (
-    <Input placeholder='Search tasks...' />
+    <Input
+      placeholder='Search tasks...'
+      value={searchValue}
+      onChange={(e) => setSearchValue(e.target.value)}
+      />
   )
 }
 
