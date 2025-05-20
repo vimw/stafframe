b490092e7556ce2a7e3c792c73c9caa1a067ba31
vimw <xavobudzio@gmail.com>
2025-05-20 11:12:11 +0200

    Changes to some css in taskview

--- app/components/TaskviewNavigation.tsx
+++ app/components/TaskviewNavigation.tsx
@@ -13,7 +13,7 @@ function TaskviewNavigation({ yday }: { yday?: number }){
         <div className={styles.navbar}>
             <p>Dailyview for: </p>
             <h2>{`${ydayDate.getDate()}-${ydayDate.getMonth()+1}-${ydayDate.getFullYear()}`}</h2>
-            <Button type="default" shape="round" size="large" href={`/dailyview/${ydayFr-1 < 1 ? 365 : ydayFr-1}`}>Prev</Button>
+            <Button type="default" shape="round" size="large" href={`/dailyview/${ydayFr-1 < 1 ? 365 : ydayFr-1}`} style={{marginRight: "0.4rem"}}>Prev</Button>
             <Button type="default" shape="round" size="large" href={`/dailyview/${ydayFr+1 > 365 ? 1 : ydayFr+1}`}>Next</Button>
         </div>
     );
