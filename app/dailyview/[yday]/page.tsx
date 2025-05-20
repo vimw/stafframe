1474f9b2e2f09dc3cfa12ca7b8a68c35c3f0c7b2
vimw <xavobudzio@gmail.com>
2025-05-20 11:24:53 +0200

    Fix error with await params

--- app/dailyview/[yday]/page.tsx
+++ app/dailyview/[yday]/page.tsx
@@ -3,7 +3,8 @@ import { Taskview } from "../../components/Taskview";
 import { loadTasksIntoTaskTabsByDate } from "@/lib/tasks/loadTasks";
 
 export default async function Home({ params }: { params: any }) {
-    const yday = parseInt(await params.yday);
+    const paramss = await params;
+    const yday = parseInt(paramss.yday);
 
   return (
     <Taskview yday={yday}>
